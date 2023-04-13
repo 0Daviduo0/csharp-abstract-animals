3b31d33a76e9a5eef29a0e76e1619524c47436ca
Daviduo <davide0400@gmail.com>
2023-04-13 16:22:59 +0200

    Add project files.

---
+++ csharp-abstract-animals/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace csharp_abstract_animals
+{
+    internal class Program
+    {
+
+        //Dobbiamo realizzare un programma che rappresenti il regno animale.
+        //Creiamo quindi una classe astratta Animale e delle classi che la estendono : Cane, Passerotto, Aquila, Delfino
+        //Vogliamo che gli animali abbiano i seguenti metodi
+        //void Dormi() (mostra a video “Zzz”)
+        //void Verso() (mostra a video il verso fatto dall'animale specifico)
+        //void Mangia() (mostra a video quello che mangia : erba, carne, ...?)
+        //Quali di questi metodi devono essere implementati nella classe astratta e quali devono essere lasciati astratti e implementati dalle classi che la estendono?
+        //Una volta create le classi, nel programma istanziare alcuni animali e utilizzare i metodi implementati.
+
+        static void Main(string[] args)
+        {
+            Cane cane = new Cane();
+            cane.Verso();
+            cane.Mangia();
+            cane.Dormi();
+
+            Passerotto passerotto = new Passerotto();
+            passerotto.Verso();
+            passerotto.Mangia();
+            passerotto.Dormi();
+
+
+            Aquila aquila = new Aquila();
+            aquila.Verso();
+            aquila.Mangia();
+            aquila.Dormi();
+
+
+            Delfino delfino = new Delfino();
+            delfino.Verso();
+            delfino.Mangia();
+            delfino.Dormi();
+
+            static void FaiVolare(iVola animals)
+            {
+                animals.Vola();
+            }
+
+            static void FaiNuotare(iNuota animals)
+            {
+                animals.Nuota();
+            }
+
+            Console.WriteLine("il passerotto sta volando: ");
+            Passerotto passerottoVolante = new Passerotto();
+            FaiVolare(passerottoVolante);
+            Console.WriteLine("l'aquila sta volando: ");
+            Aquila aquilaVolante = new Aquila();
+            FaiVolare(aquilaVolante);
+
+
+            Console.WriteLine("il delfino sta nuotando: ");
+            Delfino delfinoNuotante = new Delfino();
+            FaiNuotare(delfinoNuotante);
+        }
+    }
+}
---
+++ csharp-abstract-animals/animals.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_abstract_animals
+{
+    abstract class animal
+    {
+        public void Dormi()
+        {
+            Console.WriteLine("Zzzzzzzzzzzz");
+        }
+
+        public abstract void Verso();
+        public abstract void Mangia();
+    }
+
+    class Cane : animal
+    {
+
+        string nome = "cane";
+        public override void Verso()
+        {
+            Console.WriteLine($"il verso del {nome} è: bau bau arf arf");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine($"il {nome} è onnivoro; quindi carne, verdure, bambini");
+        }
+    }
+
+    class Passerotto : animal, iVola
+    {
+
+        string nome = "passerotto";
+        public override void Verso()
+        {
+            Console.WriteLine($"il verso del {nome} è: pio pio chip chip");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine($"il {nome} mangia: semi, granaglie, frutta, bacche e insetti");
+        }
+
+        public void Vola()
+        {
+            Console.WriteLine("Sto volando!!!");
+        }
+    }
+
+    class Aquila : animal, iVola
+    {
+
+        string nome = "aquila";
+        public override void Verso()
+        {
+            Console.WriteLine($"il verso dell'{nome} è: krak-krak-krak eek-eek-eek");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine($"il {nome} mangia: roditori, conigli, serpenti, pesci, uccelli");
+        }
+
+        public void Vola()
+        {
+            Console.WriteLine("Sto volando!!!");
+        }
+    }
+
+    class Delfino : animal, iNuota
+    {
+
+        string nome = "delfino";
+        public override void Verso()
+        {
+            Console.WriteLine($"il verso del {nome} è: iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii");
+        }
+
+        public override void Mangia()
+        {
+            Console.WriteLine($"il {nome} mangia: pesci, calamari, gamberetti, crostacei");
+        }
+
+        public void Nuota()
+        {
+            Console.WriteLine("Sto nuotando!!!");
+        }
+    }
+}
